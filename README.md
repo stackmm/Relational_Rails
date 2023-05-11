b4610e4a1dfedbcdf5b41bdc74a52de4e05aa588
Steven Stackhouse <stackmm@gmail.com>
2023-05-11 13:06:17 -0600

    complete user story 5

--- README.md
+++ README.md
@@ -58,7 +58,7 @@ When I visit '/child_table_name/:id'
 Then I see the child with that id including the child's attributes
 (data from each column that is on the child table)
 
-
+[] done
 User Story 5, Parent Children Index 
 
 As a visitor
@@ -66,7 +66,7 @@ When I visit '/parents/:parent_id/child_table_name'
 Then I see each Child that is associated with that Parent with each Child's attributes
 (data from each column that is on the child table)
 
-
+[] done
 User Story 6, Parent Index sorted by Most Recently Created 
 
 As a visitor
@@ -74,28 +74,28 @@ When I visit the parent index,
 I see that records are ordered by most recently created first
 And next to each of the records I see when it was created
 
-
+[] done
 User Story 7, Parent Child Count
 
 As a visitor
 When I visit a parent's show page
 I see a count of the number of children associated with this parent
 
-
+[] done
 User Story 8, Child Index Link
 
 As a visitor
 When I visit any page on the site
 Then I see a link at the top of the page that takes me to the Child Index
 
-
+[] done
 User Story 9, Parent Index Link
 
 As a visitor
 When I visit any page on the site
 Then I see a link at the top of the page that takes me to the Parent Index
 
-
+[] done
 User Story 10, Parent Child Index Link
 
 As a visitor
--- spec/features/pharmacies/medications/index_spec.rb
+++ spec/features/pharmacies/medications/index_spec.rb
@@ -1,23 +1,30 @@
 require "rails_helper"
 
 RSpec.describe "Pharmacies' medications index" do
-  before :each do
-    @pharmacy_1 = Pharmacy.create!(name: "Walgreens", pharmacist_in_charge: "John Smith", num_employees: 9, city: "Toronto", open_24_hours: true)
-    @medication_1 = @pharmacy_1.medications.create!(name: "Amoxicillin", strength: "500 mg", dosage_form: "tablet", quantity: 5000, in_stock: true)
-    @medication_2 = @pharmacy_1.medications.create!(name: "Penicillin VK", strength: "250 mg", dosage_form: "tablet", quantity: 400, in_stock: true)
+  let!(:pharmacy_1) {Pharmacy.create!(name: "Walgreens", pharmacist_in_charge: "John Smith", num_employees: 9, city: "Toronto", open_24_hours: true)}
+  let!(:medication_1) {pharmacy_1.medications.create!(name: "Amoxicillin", strength: "500 mg", dosage_form: "tablet", quantity: 5000, in_stock: true)}
+  let!(:medication_2) {pharmacy_1.medications.create!(name: "Penicillin VK", strength: "250 mg", dosage_form: "tablet", quantity: 400, in_stock: true)}
+  
+  # User Story 5
+  it "shows the medications associated with a specific pharmacy and their attributes" do
+    visit "/pharmacies/#{pharmacy_1.id}/medications"
+
+    expect(page).to have_content(medication_1.name)
+    expect(page).to have_content(medication_1.strength)
+    expect(page).to have_content(medication_1.dosage_form)
+    expect(page).to have_content(medication_1.quantity)
+    expect(page).to have_content(medication_1.in_stock)
+
+    expect(page).to have_content(medication_2.name)
+    expect(page).to have_content(medication_2.strength)
+    expect(page).to have_content(medication_2.dosage_form)
+    expect(page).to have_content(medication_2.quantity)
+    expect(page).to have_content(medication_2.in_stock)
   end
-  it "shows all the names of the medications for a specific pharmacy" do
-    visit "/pharmacies/#{@pharmacy_1.id}/medications"
 
-    expect(page).to have_content(@medication_1.name)
-    expect(page).to have_content(@medication_2.name)
-  end
-
-  it "links to each medication's show page" do
-    visit "/pharmacies/#{@pharmacy_1.id}/medications"
-
-    click_on @medication_1.name
-
-    expect(current_path).to eq("/medications/#{@medication_1.id}")
-  end
+  # it "links to each medication's show page" do
+  #   visit "/pharmacies/#{pharmacy_1.id}/medications"
+  #   click_on medication_1.name
+  #   expect(current_path).to eq("/medications/#{medication_1.id}")
+  # end
 end
